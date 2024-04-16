3a0159cb9d96b79848ebaf57299981ec29a42cc4
waldeng24 <166259276+waldeng24@users.noreply.github.com>
2024-04-16 19:07:34 -0400

    Add files via upload

--- OptimalPathGradient.m
+++ OptimalPathGradient.m
@@ -1,10 +1,11 @@
 function path = OptimalPathGradient2(start,goal) 
+
 FW4Plotfunction
-r = 0.25;
-%goal = [8.2 4.4 200*griewank([8.2 4.4])+1.1*r];
+r = 0.5;
+%goal = [-8.2 -4.4 griewank([-8.2 -4.4])+1.1*r];
 %start = [0 0 1.1];
 %stepsize
-alpha = 0.05;
+alpha = 0.001;
 
 
 xmin = -10;
@@ -18,7 +19,7 @@ end
 %defines obstacles repulsion and gradient mesh
 for i = 1:101
     for j =  1:101
-        Krepulsive(i,j) = 200*griewank([x1(j) x2(i)]);
+        Krepulsive(i,j) = griewank([x1(j) x2(i)]);
         [dFdx(i,j), dFdy(i,j)] = gradgriewank(x1(j),x2(i));
     end
 end
@@ -27,58 +28,45 @@ end
 
 current = start;
 %loop until you reach goal or for 10 steps
-for k=1:60000
+for k=1:30000
     %check if you reached the goal
-    dg = fxyz(goal,current);
+    dg = gxyz(goal,current);
     if dg <=0.1
         break
     end
     
     %generate gradient steps from goal for x y and z
     for ii = 1:3
-        sumdF(ii) = (current(ii)-goal(ii))/fxyz(current,goal);
+        sumdF(ii) = (current(ii)-goal(ii))/gxyz(current,goal);
     end
-
-    %check repulsive forces from each obstacle
+   
+    %locate nearest obstacle
+    dmin = 999;
+    ob = [0 0 0];
     for i = 1:101
-        for j =  1:101
-            %check distance from each obstacle
-            di(i,j) = fxyz(current,[x1(j) x2(i) Krepulsive(i,j)]); 
-            
-            if di(i,j) <= r
-                Frepulsion(i,j) = Krepulsive(i,j)*(1/di(i,j)-1/r);
-            elseif di(i,j) > 1.5*r
-                Frepulsion(i,j) = 0;
-            else
-                Frepulsion(i,j) = Krepulsive(i,j)/di(i,j)^2;
+        for j =  1:101            
+            di(i,j) = gxyz(current,[x1(j) x2(i) Krepulsive(i,j)]); 
+            if di(i,j) <dmin
+                dmin = di(i,j);
+                ob = [x1(j) x2(i) Krepulsive(i,j)];
             end
-
-            sumdF(1) = sumdF(1)+dFdx(i,j)*(current(1)-x1(j))/di(i,j)*Frepulsion(i,j);
-            sumdF(2) = sumdF(2)+dFdy(i,j)*(current(2)-x2(i))/di(i,j)*Frepulsion(i,j);
-            sumdF(3) = sumdF(3)-(current(3)-Krepulsive(i,j))/di(i,j)*Frepulsion(i,j);
         end
     end
+    sumdF(1) = sumdF(1)+(ob(1)/2000+sin(ob(1))*cos(ob(2)/sqrt(2)))*(current(1)-ob(1))/dmin*ob(3);
+    sumdF(2) = sumdF(2)+(ob(2)/2000+1/sqrt(2)*cos(ob(2))*sin(ob(2)/sqrt(2)))*(current(2)-ob(2))/dmin*ob(3);
+    sumdF(3) = sumdF(3)+(current(3)-ob(3))*ob(3)/dmin;
     path(k,:) = current;
     for ii =1:3
         dstep(k,ii) = -alpha*sumdF(ii);
-        if k > 1
-            if ii == 3
-                lim = 20*alpha;
-            else
-                lim = alpha;
-            end
-            if abs(dstep(k,ii))>lim
-                dstep(k,ii) = dstep(k-1,ii);
-            end
-        end
         
         current(ii) = current(ii)+dstep(k,ii);
-        if ii==3
-            current(ii) = max(current(ii),200*griewank([current(1) current(2)])+r);
-        end
+
     end
 end
-
+%project onto the surface
+for i= 1:length(path)
+    path(i,3) = max(path(i,3),200*griewank([path(i,1) path(i,2)]));
+end
 figure(4)
 hold on
 plot3(path(:,1),path(:,2),path(:,3),'-c',LineWidth=3)
@@ -89,6 +77,10 @@ function [dFdx dFdy] = gradgriewank(x,y)
     dFdy = 200*(y/2000+cos(x)*sin(y/sqrt(2))/sqrt(2));
 end
 
-function dist = fxyz(goal,current)
+function dist = gxyz(goal,current)
    dist = sqrt((goal(1)-current(1))^2+(goal(2)-current(2))^2+(goal(3)-current(3))^2);
+end
+
+function dist = fxyz(goal,current)
+   dist = sqrt((goal(1)-current(1))^2+(goal(2)-current(2))^2);
 end
--- getwaypoints.m
+++ getwaypoints.m
@@ -1,14 +1,16 @@
 clear
 r = 0.25;
-goal = [8.2 4.4 200*griewank([8.2 4.4])+1.1*r];
+goal = [5 -10 griewank([-5 -10])];
 start = [0 0 1.1];
 path = OptimalPathGradient(start,goal);
 
-numpts = 100;
+numpts = 25;
 step = floor(length(path)/numpts);
 waypoints(1,:) = start;
-waypoints(numpts+1,:) = goal;
-for i = 2:numpts
-    
+waypoints(numpts+1,:) = path(length(path),:);
+for i = 1:numpts
     waypoints(i,:) = path(i*step,:);
 end
+
+figure(1)
+plot3(waypoints(:,1),waypoints(:,2),waypoints(:,3))
